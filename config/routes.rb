6556a1cb4fc356a37ad022af957c9f0a94e5686e
Klemen Nagode <klemen.nagode@gmail.com>
2017-04-12 09:49:58 +0200

    base

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :transactions, only: [:index, :create, :show, :update] do
+        resources :bank_guarantees, only: [:index, :create, :show, :update]
+      end
+    end
+  end
+
+end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+
+  factory :transaction do
+    name "New Transaction"
+    active true
+  end
+
+end
